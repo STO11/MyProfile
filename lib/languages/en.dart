ecaf9990ea50178f598728f38f2698813532d694
Suelton <sueltonlima@gmail.com>
2022-03-03 00:57:11 -0300

    refactor: remove trash code

--- lib/languages/en.dart
+++
@@ -1,5 +0,0 @@
-mixin EnLanguage {
-  final String aboutMeEn =
-      "I'm a senior PHP developer with over 10 years of experience in back-end and 7 years with mobile, programming since I was 16 years old. I have extensive experience in analysis, system development and application creation using the best technologies. I worked as a fullstack in several technologies and currently working with react native on a platform with millions of unique users.";
-  final String myTechnicalSkillsSectionEn = 'Tecnical Skills';
-}
